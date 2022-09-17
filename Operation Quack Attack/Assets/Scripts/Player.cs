0ce84c44abbf6a14f52253cd3721538072c279ae
KSVasquez <ksv3212@rit.edu>
2022-09-17 05:44:15 -0400

    implements jumping, revamps lateral movement

--- Operation Quack Attack/Assets/Scripts/Player.cs
+++ Operation Quack Attack/Assets/Scripts/Player.cs
@@ -16,48 +16,95 @@ public class Player : MonoBehaviour
     [SerializeField] private float accelRate = 10;
     const float deccel = 0.75f;
 
-    SpriteRenderer sprite;
+    [SerializeField] private float jumpStrength = 10;
+    [SerializeField] private float fallGravity = 15;
+    [SerializeField] private float jumpGravity = 10;
+    private bool onGround = false;
+    [SerializeField] private float driftSpeed = 5;
+    const float airDeccel = 0.95f;
+
+
+    SpriteRenderer spr;
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         pos = transform.position;
         speed = baseSpeed;
-        sprite = GetComponent<SpriteRenderer>();
+        spr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Get current position
-        pos = transform.position;
+        //pos = transform.position;
 
-        // Lateral movement
-        if (input.x != 0)
+        // Ground Movement
+        if (onGround)
         {
-            // Reset speed when changing directions
-            if (input.x + oldInput.x == 0)
+            spr.color = Color.white;
+
+            // Lateral movement
+            if (input.x != 0)
             {
-                speed = baseSpeed;
+                // Reset speed when changing directions
+                if (input.x + oldInput.x == 0)
+                {
+                    speed = baseSpeed;
+                }
+                // Accelerate speed, clamped from baseSpeed to topSpeed
+                speed = Mathf.Clamp(speed + accelRate * Time.deltaTime, baseSpeed, topSpeed);
+                // Update velocity
+                vel.x = input.x * speed;
             }
-            // Accelerate speed, clamped from baseSpeed to topSpeed
-            speed = Mathf.Clamp(speed + accelRate * Time.deltaTime, baseSpeed, topSpeed);
-            // Update velocity
-            vel.x = input.x * speed;
+            else
+            {
+                // Reset speed and deccelerate velocity
+                speed = baseSpeed;
+                vel.x *= deccel;
+                if (vel.x < baseSpeed * 0.1f) vel.x = 0;
+            }
         }
+        // Air Movement
         else
         {
-            // Reset speed and deccelerate velocity
-            speed = baseSpeed;
-            vel.x *= deccel;
-            if (vel.magnitude < baseSpeed * 0.1f) vel = Vector2.zero;
+            spr.color = Color.red;
+
+            // Vertical movement
+            if (vel.y > 0)
+            {
+                vel.y -= jumpGravity * Time.deltaTime;
+            }
+            else
+            {
+                vel.y -= fallGravity * Time.deltaTime;
+            }
+
+            // Lateral movement
+            if (input.x != 0)
+            {
+                // Update velocity
+                Vector2 latVel = new Vector2(vel.x + input.x * driftSpeed * Time.deltaTime, 0);
+                latVel = Vector2.ClampMagnitude(latVel, topSpeed);
+                vel.x = latVel.x;
+            }
+            else
+            {
+                // Reset speed and deccelerate velocity
+                vel.x *= airDeccel;
+
+            }
         }
 
         // Apply velocity
         pos += vel * Time.deltaTime;
 
         // Update position
-        transform.position = pos;
+        //transform.position = pos;
+        rb.velocity = vel;
     }
 
     private void OnMove(InputValue value)
@@ -70,11 +117,55 @@ public class Player : MonoBehaviour
 
     private void OnJump(InputValue value)
     {
-        //vel.y += 10;
+        if (onGround)
+        {
+            vel.y = jumpStrength;
+            onGround = false;
+        }
     }
 
     private void OnDash(InputValue value)
     {
         // TBD
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.bounds.max.y < collision.otherCollider.bounds.min.y)
+        {
+            onGround = true;
+            if (vel.y < 0)
+            {
+                vel.y = 0;
+            }
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.bounds.max.y < collision.otherCollider.bounds.min.y)
+        {
+            onGround = true;
+        }
+        else
+        {
+            if (!onGround)
+            {
+                speed = baseSpeed;
+                vel.x *= deccel;
+                if (vel.x < baseSpeed * 0.1f) vel.x = 0;
+                spr.color = Color.blue;
+            }
+
+            if (vel.y > 0)
+            {
+                vel.y *= deccel;
+                spr.color = Color.blue;
+            }
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        onGround = false;
     }
 }
