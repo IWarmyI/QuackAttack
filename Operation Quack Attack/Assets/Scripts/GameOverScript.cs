9e6cc7ccdca25b31857f7810322e8d90b784a72c
KSVasquez <ksv3212@rit.edu>
2022-10-06 13:49:03 -0400

    enables controller Menu UI for Game and Tutorial

--- Operation Quack Attack/Assets/Scripts/GameOverScript.cs
+++ Operation Quack Attack/Assets/Scripts/GameOverScript.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class GameOverScript : MonoBehaviour
 {
+    public GameObject firstOption;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstOption);
     }
 
     // Update is called once per frame
--- Operation Quack Attack/Assets/Scripts/tempTutorialLaunch.cs
+++ Operation Quack Attack/Assets/Scripts/tempTutorialLaunch.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class tempTutorialLaunch : MonoBehaviour
 {
+    public GameObject firstOption;
+
+    private void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstOption);
+    }
+
     public void GoToTutorial()
     {
         //Play Tutorial
