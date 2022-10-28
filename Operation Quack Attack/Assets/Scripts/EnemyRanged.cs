8cb0f1605c998f162c92edd50d6595641258f3e1
Darvin Muralitharan <muralitharan.darvin@gmail.com>
2022-10-27 22:55:53 -0400

    Build Bug

--- Operation Quack Attack/Assets/Scripts/EnemyRanged.cs
+++ Operation Quack Attack/Assets/Scripts/EnemyRanged.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
+//using static UnityEditor.PlayerSettings;
 
 public class EnemyRanged : Enemy
 {
