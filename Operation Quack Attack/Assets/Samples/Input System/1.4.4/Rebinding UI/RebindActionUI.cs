336545c10b88c1f848ed64fd64c78799f9f82ea2
Darvin Muralitharan <muralitharan.darvin@gmail.com>
2023-04-09 02:21:34 -0400

    Rebinds hopefully stick now

--- Operation Quack Attack/Assets/Samples/Input System/1.4.4/Rebinding UI/RebindActionUI.cs
+++ Operation Quack Attack/Assets/Samples/Input System/1.4.4/Rebinding UI/RebindActionUI.cs
@@ -262,11 +262,17 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                 m_RebindOperation = null;
             }
 
+            // Disable action before use
+            action.Disable();
+
             // Configure the rebind.
             m_RebindOperation = action.PerformInteractiveRebinding(bindingIndex)
+                .WithControlsExcluding("<Mouse>")
+                .WithControlsExcluding("<Pointer>/position")
                 .OnCancel(
                     operation =>
                     {
+                        action.Enable();
                         m_RebindStopEvent?.Invoke(this, operation);
                         m_RebindOverlay?.SetActive(false);
                         UpdateBindingDisplay();
@@ -275,6 +281,7 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                 .OnComplete(
                     operation =>
                     {
+                        action.Enable();
                         m_RebindOverlay?.SetActive(false);
                         m_RebindStopEvent?.Invoke(this, operation);
                         UpdateBindingDisplay();
@@ -421,6 +428,11 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
         }
 
         #endif
+
+        private void Start() {
+            UpdateActionLabel();
+            UpdateBindingDisplay();
+        }
 
         private void UpdateActionLabel()
         {
